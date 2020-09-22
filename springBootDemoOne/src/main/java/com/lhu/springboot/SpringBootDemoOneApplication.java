f93cc11f6e9d7dba49829091b1e6209124dd0742
Lahiru Priyankara <priyankaraggl@gmail.com>
2020-09-22 23:10:37 +0530

    First commit

---
+++ springBootDemoOne/src/main/java/com/lhu/springboot/SpringBootDemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.lhu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoOneApplication.class, args);
+	}
+
+}
